2e489a75f30706285d720f752160b59229baab94
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-03-31 11:37:39 +0200

    Add: Manifest.json

--- Week3/public/sw.js
+++ Week3/public/sw.js
@@ -5,7 +5,7 @@
 const config = {
   CORE_CACHE_NAME: 'shirtmaker-v1.0.0-core', // Name of cache with all core files
   PAGE_CACHE_NAME: 'shirtmaker-v1.0.0-pages', // Name of cache with all catched pages
-  CORE_FILES: ['/assets/css/style.css'] // Core files to cache
+  CORE_FILES: ['/assets/css/style.css', '/assets/img/shirt.png'] // Core files to cache
 }
 
 /* CACHE CORE SITE ASSETS
